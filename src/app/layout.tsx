06bdc622e0d19cd2d27b31ba975f9dda43340cb7
Fabricio <fabricio-back@github.com>
2026-02-09 14:14:39 -0300

    feat: adicionar Google Ads tag (gtag.js) AW-302827508

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -259,6 +259,21 @@ export default function RootLayout({
   return (
     <html lang="pt-BR">
       <head>
+        {/* Google tag (gtag.js) */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=AW-302827508"
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'AW-302827508');
+            `,
+          }}
+        />
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(structuredData) }}
